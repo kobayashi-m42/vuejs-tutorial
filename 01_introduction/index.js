26ae61e159e473f7627a84f218091b7ec9f148cf
kobayashi-m42 <m42kobayashi@gmail.com>
2018-08-09 14:56:34 +0900

    コンポーネントによる構成を追加

--- 01_introduction/index.js
+++ 01_introduction/index.js
@@ -51,3 +51,19 @@ const app6 = new Vue({
     message: 'Hello Vue!'
   }
 });
+
+Vue.component('todo-item', {
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>'
+});
+
+const app7 = new Vue({
+  el: '#app-7',
+  data: {
+    groceryList: [
+      { id: 0, text: 'Vegetables' },
+      { id: 1, text: 'Cheese' },
+      { id: 2, text: 'Whatever else humans are supposed to eat' }
+    ]
+  }
+});
