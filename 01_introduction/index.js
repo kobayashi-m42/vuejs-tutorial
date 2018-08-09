c87eb2ce6df8438478d64bf34ebf807422a79239
kobayashi-m42 <m42kobayashi@gmail.com>
2018-08-09 14:32:27 +0900

    条件分岐とループを追加

--- 01_introduction/index.js
+++ 01_introduction/index.js
@@ -11,3 +11,24 @@ const app2 = new Vue({
     message: 'You loaded this page on ' + new Date().toLocaleString()
   }
 });
+
+const app3 = new Vue({
+  el: '#app-3',
+  data: {
+    seen: true
+    // seen: false  // メッセージが消える
+  }
+});
+
+const app4 = new Vue({
+  el: '#app-4',
+  data: {
+    todos: [
+      { text: 'Learn JavaScript' },
+      { text: 'Learn Vue' },
+      { text: 'Build something awesome' }
+    ]
+  }
+});
+// コンソールで以下を追加するとリストに追加されることを確認できる
+// app4.todos.push({ text: 'New item' })
