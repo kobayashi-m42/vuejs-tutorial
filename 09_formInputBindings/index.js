31a1b718d1e84df5ae133a38c25e8c553fbc513e
kobayashi-m42 <m42kobayashi@gmail.com>
2018-08-10 22:05:27 +0900

    フォーム入力バインディングのコードを追加

---
+++ 09_formInputBindings/index.js
@@ -0,0 +1,72 @@
+const example1 = new Vue({
+  el: '#example-1',
+  data: {
+    message: ''
+  }
+});
+
+const example2 = new Vue({
+  el: '#example-2',
+  data: {
+    message: ''
+  }
+});
+
+const exampl3 = new Vue({
+  el: '#example-3',
+  data: {
+    checked: false,
+    checkedNames: []
+  }
+});
+
+const example4 = new Vue({
+  el: '#example-4',
+  data: {
+    picked: ''
+  }
+});
+
+const example5 = new Vue({
+  el: '#example-5',
+  data: {
+    selected: ''
+  }
+});
+
+const example6 = new Vue({
+  el: '#example-6',
+  data: {
+    selected: []
+  }
+});
+
+const example7 = new Vue({
+  el: '#example-7',
+  data: {
+    selected: 'A',
+    options: [
+      { text: 'One', value: 'A' },
+      { text: 'Two', value: 'B' },
+      { text: 'Three', value: 'C' }
+    ]
+  }
+});
+
+const example8 = new Vue({
+  el: '#example-8',
+  data: {
+    toggle: '',
+    a:'A',
+    b:'B',
+    pick: 'b',
+    selected: ''
+  }
+});
+
+const example9 = new Vue({
+  el: '#example-9',
+  data: {
+    msg: ''
+  }
+});
