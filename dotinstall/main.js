c2c22282c176660cc9e5b7da2f08aec9972d637a
kobayashi-m42 <m42kobayashi@gmail.com>
2018-08-14 13:47:59 +0900

    todoの追加処理を追加

--- dotinstall/main.js
+++ dotinstall/main.js
@@ -2,10 +2,17 @@ const vm = new Vue({
   el: '#app',
   data: {
     name: 'taguchi',
+    newItem: '',
     todos: [
       'task 1',
       'task 2',
       'task 3'
     ]
+  },
+  methods: {
+    addItem: function () {
+      this.todos.push(this.newItem);
+      this.newItem = '';
+    }
   }
 });
